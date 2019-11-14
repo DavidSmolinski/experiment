c60c088c5fc26b3979dda34f0eec414f6fcd783a
David Smolinski <dsmolinski123@gmail.com>
2019-11-13 21:21:17 -0500

    Initial Commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+
+
+import sys
+
+print("\n\nhi\n\n")
+print(f'version: {sys.version}')
+print(f'exe: {sys.executable}')
+a=1
+a = (True, True, True, True, True, True, True, True, True,
+     True, True, True, True, True, True, True, True, True)
+
+print(a)
